5329b80113b63d8e5ad8c1f5a9813e90e13f21de
Giovane <giovanedaniell@gmail.com>
2023-05-24 00:03:35 -0300

    feat: store text input value in a state on change

--- App.tsx
+++ App.tsx
@@ -1,14 +1,37 @@
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { useEffect, useState } from "react";
+import {
+  Button,
+  NativeSyntheticEvent,
+  StyleSheet,
+  Text,
+  TextInput,
+  TextInputChangeEventData,
+  View,
+} from "react-native";
 
 export default function App() {
+  const [goal, setGoal] = useState<string>("");
+
+  function handleInputChange(text: string) {
+    setGoal(text);
+  }
+
+  function handleAddGoalPress() {
+    console.log("pressed");
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Enter a goal!" />
-        <Button title="Add goal" />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Enter a goal!"
+          onChangeText={handleInputChange}
+        />
+        <Button title="Add goal" onPress={handleAddGoalPress} />
       </View>
 
-      <View>
+      <View style={styles.goalsList}>
         <Text>List of goals</Text>
       </View>
     </View>
@@ -17,15 +40,20 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    padding: 50,
-    alignItems: "center",
-    justifyContent: "center",
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
 
   inputContainer: {
+    flex: 1,
     flexDirection: "row",
-    justifyContent: "space-between",
+    justifyContent: "center",
     gap: 8,
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
   },
 
   textInput: {
@@ -33,5 +61,10 @@ const styles = StyleSheet.create({
     borderColor: "#ccc",
     width: "80%",
     paddingHorizontal: 8,
+    paddingVertical: 2,
+  },
+
+  goalsList: {
+    flex: 4,
   },
 });
