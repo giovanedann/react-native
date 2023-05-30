7197de19f2dc9294e64781b4b7c14f0ed1ef4d4e
Giovane <giovanedaniell@gmail.com>
2023-05-29 21:35:24 -0300

    feat(screens): add background image to GameStart

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, ImageBackground } from "react-native";
 import GameStart from "./src/screens/GameStart";
 import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
   return (
     <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.root}>
-      <GameStart />
+      <ImageBackground
+        source={require("./assets/background.png")}
+        resizeMode="cover"
+        style={styles.root}
+        imageStyle={styles.backgroundImage}
+      >
+        <GameStart />
+      </ImageBackground>
     </LinearGradient>
   );
 }
@@ -14,5 +21,9 @@ const styles = StyleSheet.create({
   root: {
     flex: 1,
     height: "100%",
+  },
+
+  backgroundImage: {
+    opacity: 0.3,
   },
 });
