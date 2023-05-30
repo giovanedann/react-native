ff84634f3c428703e7a1feefa17d4066cb6cb910
Giovane <giovanedaniell@gmail.com>
2023-05-29 21:50:55 -0300

    feat(screens): add alerts on invalid input values of GameStart

--- src/screens/GameStart/index.tsx
+++ src/screens/GameStart/index.tsx
@@ -1,8 +1,30 @@
-import { TextInput, View } from "react-native";
+import { TextInput, View, Alert } from "react-native";
 import styles from "./styles";
 import Button from "../../components/Button";
+import { useState } from "react";
 
 export default function GameStart() {
+  const [enteredNumber, setEnteredNumber] = useState<string>("");
+
+  function onConfirm() {
+    const convertedEnteredNumber = Number(enteredNumber);
+
+    const isNumberInvalid =
+      typeof convertedEnteredNumber !== "number" || convertedEnteredNumber <= 0;
+
+    if (isNumberInvalid) {
+      Alert.alert(
+        "Invalid number!",
+        "Number must be an integer number between 1 and 99",
+        [{ text: "Understood!", style: "destructive", onPress: onReset }]
+      );
+    }
+  }
+
+  function onReset() {
+    setEnteredNumber("");
+  }
+
   return (
     <View style={styles.inputContainer}>
       <TextInput
@@ -11,14 +33,16 @@ export default function GameStart() {
         keyboardType="number-pad"
         autoCapitalize="none"
         autoCorrect={false}
+        onChangeText={(text) => setEnteredNumber(text)}
+        value={enteredNumber}
       />
       <View style={styles.buttonsContainer}>
         <View style={styles.buttonContainer}>
-          <Button title="Lower" />
+          <Button title="Reset" onPress={onReset} />
         </View>
 
         <View style={styles.buttonContainer}>
-          <Button title="Higher" />
+          <Button title="Confirm" onPress={onConfirm} />
         </View>
       </View>
     </View>
