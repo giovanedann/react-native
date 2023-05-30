6341cc759a4e2fb289bd875224cc31a593e9adbe
Giovane <giovanedaniell@gmail.com>
2023-05-29 21:44:07 -0300

    feat(components): add onPress prop to Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,12 +3,14 @@ import styles from "./styles";
 
 type ButtonProps = {
   title: string;
+  onPress: () => void;
 };
 
-export default function Button({ title }: ButtonProps) {
+export default function Button({ title, onPress }: ButtonProps) {
   return (
     <View style={styles.wrapper}>
       <Pressable
+        onPress={onPress}
         style={({ pressed }) =>
           pressed ? [styles.container, styles.pressed] : styles.container
         }
