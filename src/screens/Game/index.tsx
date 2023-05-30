992ec39c344c175fe13cdb511d48474c646ee87b
Giovane <giovanedaniell@gmail.com>
2023-05-29 23:21:00 -0300

    feat(screens): add buttons to tell if computer guessed the wrong number on Game

--- src/screens/Game/index.tsx
+++ src/screens/Game/index.tsx
@@ -1,23 +1,49 @@
-import { Text, View } from "react-native";
+import { Alert, Text, View } from "react-native";
 import styles from "./styles";
 import Title from "../../components/Title";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import generateRandomNumberBetween from "../../utils/generateRandomNumberBetween";
 import GuessedNumber from "../../components/GuessedNumber";
+import Button from "../../components/Button";
 
 type GameProps = {
   userPickedNumber: number;
 };
 
+let minBoundary = 1;
+let maxBoundary = 100;
+
 export default function Game({ userPickedNumber }: GameProps) {
   const initialComputerGuess = generateRandomNumberBetween(
-    1,
-    100,
+    minBoundary,
+    maxBoundary,
     userPickedNumber
   );
 
   const [currentComputerGuess, setCurrentComputerGuess] =
     useState<number>(initialComputerGuess);
+
+  function guessNewNumber(direction: "lower" | "higher") {
+    const isUserLying =
+      (direction === "lower" && currentComputerGuess < userPickedNumber) ||
+      (direction === "higher" && currentComputerGuess > userPickedNumber);
+
+    if (isUserLying) {
+      return Alert.alert("Do not lie!!", "You know this is wrong...", [
+        { text: "Sorry!", style: "cancel" },
+      ]);
+    }
+
+    if (direction === "lower") {
+      maxBoundary = currentComputerGuess;
+    } else {
+      minBoundary = currentComputerGuess + 1;
+    }
+
+    setCurrentComputerGuess((prev) =>
+      generateRandomNumberBetween(minBoundary, maxBoundary, prev)
+    );
+  }
 
   return (
     <View style={styles.container}>
@@ -27,6 +53,10 @@ export default function Game({ userPickedNumber }: GameProps) {
 
       <View>
         <Text>Higher or lower?</Text>
+        <View>
+          <Button title="Lower" onPress={() => guessNewNumber("lower")} />
+          <Button title="Higher" onPress={() => guessNewNumber("higher")} />
+        </View>
       </View>
 
       <View>
